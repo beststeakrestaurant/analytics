24ef8fe820fea2350c7f02d8efce5c85819bb708
Silviu Rosu <silviu.rosu@gmail.com>
2016-03-15 10:12:43 +0200

    remove cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :analytics, Analytics.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "dinchi-analytics-phoenix.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json",
+  # cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :logger, level: :error
